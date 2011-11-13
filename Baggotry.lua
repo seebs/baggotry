dda88162403c821c3305dbe60949d3a5d90046d7
seebs <seebs@seebs.net>
2011-11-13 12:36:42 -0600

    Added rarity, added demo of .iterate

--- Baggotry.lua
+++ Baggotry.lua
@@ -14,12 +14,21 @@ function bag.printf(fmt, ...)
   print(string.format(fmt or 'nil', ...))
 end
 
+function valuation(details, slot, value)
+  value = value or 0
+  if details.sell then
+    value = value + details.sell
+  end
+  return value
+end
+
 function bag.slashcommand(args)
-  local merge, split
+  local stack = false
   local filter
-  merge = false
-  split = false
   local stack_size = nil
+  if not args then
+    return
+  end
   if args['v'] then
     bag.printf("version %s", bag.version)
     return
@@ -37,36 +46,98 @@ function bag.slashcommand(args)
     filter:slot(Utility.Item.Slot.Inventory())
   end
 
+  if args['d'] then
+    filter:descr(args['d'])
+  end
+
   if args['x'] then
     filtery = function(...) filter:exclude(...) end
   else
-    filtery = function(...) filter:include(...) end
+    if args['r'] then
+      filtery = function(...) filter:require(...) end
+    else
+      filtery = function(...) filter:include(...) end
+    end
   end
 
-  if args['n'] then
-    stack_size = args['n']
+  if args['l'] then
+    local ordered = {}
+    for k, v in pairs(bag.filters) do
+      table.insert(ordered, k)
+    end
+    table.sort(ordered)
+    for _, v in ipairs(ordered) do
+      bag.printf("Filter %s:", v)
+      bag.filters[v]:dump()
+    end
+    return
   end
 
   if args['c'] then
     filtery('category', args['c'])
   end
+  if args['q'] then
+    if lbag.rarity_p(args['q']) then
+      filtery('rarity', args['q'])
+    else
+      bag.printf("Error: '%s' is not a valid rarity.", args['q'])
+    end
+  end
   for k, item_name in pairs(args['leftover_args']) do
     filtery('name', item_name)
   end
 
-  if args['M'] then
-    merge = true
-  end
   if args['S'] then
-    split = true
+    stack = true
+    stack_size = args['S']
   end
-  if merge then
-    lbag.merge(filter, stack_size)
-  elseif split then
-    lbag.split(filter, stack_size)
+  if args['D'] then
+    filter:dump()
+    return
+  end
+  if args['s'] then
+    local total, count
+
+    total, count = lbag.iterate(filter, valuation)
+
+    local silver = total % 100
+    local gold = math.floor(total / 100)
+    local plat = math.floor(gold / 100)
+    gold = gold % 100
+    if plat > 0 then
+      bag.printf("%d item(s), total value: %dp%dg%ds.", count, plat, gold, silver)
+    elseif gold > 0 then
+      bag.printf("%d item(s), total value: %dg%ds.", count, gold, silver)
+    elseif silver > 0 then
+      bag.printf("%d item(s), total value: %ds.", count, silver)
+    else
+      bag.printf("Total value: none.")
+    end
+    return
+  end
+  if stack then
+    lbag.stack(filter, stack_size)
   else
     lbag.dump(filter)
   end
 end
 
-Library.LibGetOpt.makeslash("c:f:MSn#vx", "Baggotry", "bag", bag.slashcommand)
+local f
+
+f = lbag.filter()
+f:require('category', 'collectible')
+f:require('stackMax', 99)
+f:descr("Collectibles (such as artifacts)")
+bag.filters['a'] = f
+
+f = lbag.filter()
+f:include('category', 'material')
+f:descr("Materials")
+bag.filters['m'] = f
+
+f = lbag.filter()
+f:exclude('rarity', 'common')
+f:descr("Trash (grey items)")
+bag.filters['t'] = f
+
+Library.LibGetOpt.makeslash("c:d:Df:lq:rsS#vx", "Baggotry", "bag", bag.slashcommand)
--- ReadMe.txt
+++ ReadMe.txt
@@ -5,27 +5,47 @@ also occasionally useful, since autosplitting and automerging are two commonly
 requested features.
 
 Usage:
-	/bag [-f filter] [-n count] [-S|-M] [-c category] [name ...]
+	/bag [-f filter] [-rx] [-l] [-SD] [-c category] [-q rarity] [name ...]
 
 [-f filter] lets you name filters; "filter" can be a string and is used
-as an index into a table.  Modifiers like "-c category" or "name" are
-added to filter as :includes (see LibBaggotry for explanations).  You
-can then use -f filter without having to type the stuff out.  Filters
-are not currently saved.
+as an index into a table, and modifiers are stored in that filter, which
+then persists through the rest of your session.  If you do not specify
+a filter, each /bag invocation starts a brand new filter.
+
+Modifiers like "-c category" or "name" are added to the filter, as :includes
+by default, or as :requires (-r) or :excludes (-x).  See LibBaggotry for
+explanations.
 
 The default is just to dump the list of items found.  The interesting
-part is the -M (merge) and -S (split) options.
-	/bag -M -c crafting
+part is the -S (stack) option.  This tries to move things into stacks of
+a specified size.  If the size specified is less than 1, it counts down
+from the maximum stack size for the object; for instance, a stack size of
+0 is the same as "whatever size the object stacks to".
+
+	/bag -S 0 -c crafting
 
 Tries to combine all things tagged as "crafting" into the largest stacks
 it can.
 
-	/bag -n 1 -S -c collectible
+	/bag -S 1 -c collectible
 
-Tries to split all collectibles (such as artifacts) into stacks of 1.
+Tries to split all collectibles (such as artifacts) into stacks of 1.  Of
+course, you could use a built-in filter:
 
-	/bag -n 10 -S -c material
+	/bag -S 1 -f a
 
-Tries to split all "materials" into stacks of 10 -- which actually
-implies merging them if you have smaller stacks.
+How would you know about that?  Why, you'd list the filters:
 
+	/bag -l
+
+I'm hoping to add some clever way to save filters, but it's obviously
+impossible in the general case, because you could provide a function
+reference as part of a filter, and there's no way for me to save that
+properly.
+
+You can dump a specific filter:
+	/bag -D -f a
+
+and see what rules that filter uses.
+
+You can sum the value of a filter with the -s option.
